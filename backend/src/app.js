f1bd97e43365d761f7accf2aa868c0596f944039
Shardendu Kumar <ohmsingh100@gmail.com>
2026-07-25 10:06:14 +0530

    Update CORS for production

--- backend/src/app.js
+++ backend/src/app.js
@@ -10,7 +10,8 @@ const app = express();
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin:
+      "https://ai-interview-platform-pdroh116e-shardendu930s-projects.vercel.app",
     credentials: true,
   }),
 );
